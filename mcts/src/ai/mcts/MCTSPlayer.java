e091dda3046d6b49b1375df478af2a66205991c2
tpepels <tpepels@gmai.com>
2014-01-13 20:32:23 +0100

    try 2

--- mcts/src/ai/mcts/MCTSPlayer.java
+++ mcts/src/ai/mcts/MCTSPlayer.java
@@ -183,7 +183,7 @@ public class MCTSPlayer implements AIPlayer, Runnable {
         }
 
         // Reset the currently computed covariances
-        // options.currentCov.reset();
+        options.currentCov.reset();
 
         // Set the root to the best child, so in the next move, the opponent's move can become the new root
         if (options.treeReuse)
