c4268338ba15bf8d0f9bff88104eb54d378d2c20
Marc Lanctot <marc.lanctot@maastrichtuniversity.nl>
2014-01-16 10:56:03 +0100

    first push of alpha beta package

---
+++ alphabeta/src/alphabeta/ABMove.java
@@ -0,0 +1,12 @@
+package alphabeta;
+
+public class ABMove {
+    public final int position;
+    public int history;
+
+    //
+    public ABMove(int position) {
+        this.position = position;
+        history = 0;
+    }
+}
---
+++ alphabeta/src/alphabeta/Transposition.java
@@ -0,0 +1,12 @@
+package alphabeta;
+
+import ai.framework.IMove;
+
+public class Transposition {
+    public static final int REAL = 0, L_BOUND = 1, U_BOUND = 2;
+
+    public long hash;
+    public double value;
+    public int depth, flag; 
+    public IMove bestMove;
+}
