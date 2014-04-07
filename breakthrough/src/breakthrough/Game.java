77569ee0dd8027e6cfbdc9aa11ea6a86029ce750
Tom Pepels <tpepels@gmail.com>
2014-04-07 18:05:28 +0200

    new rb

--- breakthrough/src/breakthrough/Game.java
+++ breakthrough/src/breakthrough/Game.java
@@ -20,7 +20,7 @@ public class Game {
         options1.fixedSimulations = true;
         options1.simulations = 25000;
         options1.range_back = true;
-        options1.bp_range = .5;
+        options1.bp_range = .2;
 
         MCTSOptions options2 = new MCTSOptions();
         options2.setGame("breakthrough");
