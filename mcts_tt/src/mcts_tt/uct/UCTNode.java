2eeccd915a9f6f86d62faecbe235a349f84e22fd
Tom Pepels <tpepels@gmail.com>
2014-05-13 22:51:09 +0200

    ph

--- mcts_tt/src/mcts_tt/uct/UCTNode.java
+++ mcts_tt/src/mcts_tt/uct/UCTNode.java
@@ -212,13 +212,10 @@ public class UCTNode {
                 uctValue = -State.INF + MCTSOptions.r.nextDouble();
             } else {
                 value = c.getValue();
-//                if (options.progHistory)
-//                    value = options.phW * value + (1. - options.phW) * value;
                 // Compute the uct value with the (new) average value
                 uctValue = value + options.uctC * Math.sqrt(FastLog.log(np) / nc);
                 if (options.progHistory) {
-//                    uctValue += c.move.getHistoryVal(player, options) * (options.phW / (getVisits() - getWins() + 1));
-                    uctValue = options.phW * uctValue + (1. - options.phW) * uctValue;
+                    uctValue += c.move.getHistoryVal(player, options) * (options.phW / (getVisits() - getWins() + 1));
                 }
             }
             // Remember the highest UCT value
