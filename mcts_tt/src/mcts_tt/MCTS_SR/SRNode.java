3c20e03890ffa27b98b2398a326e29f75469de12
Tom Pepels <tpepels@gmail.com>
2014-06-06 13:05:23 +0200

    ublb remove nodes like in solver

--- mcts_tt/src/mcts_tt/MCTS_SR/SRNode.java
+++ mcts_tt/src/mcts_tt/MCTS_SR/SRNode.java
@@ -227,10 +227,20 @@ public class SRNode {
                 if (plStats[3] >= budget)
                     break;
             }
-            for (Iterator<SRNode> iterator = S.iterator(); iterator.hasNext(); ) {
-                SRNode node = iterator.next();
-                if (node.isSolved()) {
-                    iterator.remove();
+            if (options.solver) {
+                for (Iterator<SRNode> iterator = S.iterator(); iterator.hasNext(); ) {
+                    SRNode node = iterator.next();
+                    if (node.isSolved()) {
+                        iterator.remove();
+                    }
+                }
+            }
+            if (options.UBLB) {
+                for (Iterator<SRNode> iterator = S.iterator(); iterator.hasNext(); ) {
+                    SRNode node = iterator.next();
+                    if (node.getValue() + Math.sqrt((2 * FastLog.log(getVisits())) / node.getVisits()) < lb) {
+                        iterator.remove();
+                    }
                 }
             }
             // :: Removal policy: Sorting
