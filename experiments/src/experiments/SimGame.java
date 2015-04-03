7cd2dac983c1dadf6de5e8047e946791da522968
Tom Pepels <tpepels@gmail.com>
2015-04-03 18:54:25 +0200

    domineering reference

--- experiments/src/experiments/SimGame.java
+++ experiments/src/experiments/SimGame.java
@@ -420,7 +420,6 @@ public class SimGame {
         } else if (game.startsWith("phantomdomineering")) {
             int size = Integer.parseInt(game.substring(18));
             board = new phantomdomineering.game.Board(size);
-            domineering.game.Board.CRAM = false;
         } else if (game.startsWith("cram")) {
             int size = Integer.parseInt(game.substring(4));
             board = new domineering.game.Board(size);
